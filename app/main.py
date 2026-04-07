de3dbf88bbf759bb0687f2b68c4057d0710eb59e
Felipe <felipenn27@gmail.com>
2026-04-07 12:16:46 -0300

    feat(api-docs): implementa Scalar para documentacao e testes de endpoints

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 from connection import engine
 from models.db_models import SQLModel
 from api.endpoints import router as api_router
@@ -19,6 +20,17 @@ app = FastAPI(
 
 # --- INCLUINDO AS ROTAS AQUI ---
 app.include_router(api_router)
+
+
+# -_-_-_- SCALAR DOCS & API Testing -_-_-_-
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title=app.title,
+    )
+# -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
 
 @app.get("/")
 def health_check():
